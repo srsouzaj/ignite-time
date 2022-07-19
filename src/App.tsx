f874ad5b3c6c582c49530a71939ec7e37392382c
Jorge de Souza J Junior <jorgedesouza@recrutae.com.br>
2022-07-18 21:24:46 -0300

    feat: global theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { Button } from "./components/Button";
 
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />;
+      <Button variant="primary" />
+      <GlobalStyle />
     </ThemeProvider>
   );
 }
