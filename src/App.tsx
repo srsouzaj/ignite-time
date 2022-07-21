ed90dd086e9a83101f90f3b40cb6e90c97127098
Jorge de Souza J Junior <jorgedesouza@recrutae.com.br>
2022-07-21 20:42:38 -0300

    feat: adicionando navegação no projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { Button } from './components/Button'
-
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <GlobalStyle />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <Router />
+        <GlobalStyle />
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }
---
+++ src/Router.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { History } from './pages/History'
+import { DefaultLayout } from './layouts/DefaultLayouts'
+
+export const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
+    </Routes>
+  )
+}
