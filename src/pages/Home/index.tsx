46b57189320cded463ff54f3d748d80d1998cca2
Jorge de Souza J Junior <jorgedesouza@recrutae.com.br>
2022-07-21 22:30:39 -0300

    feat: history page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,4 @@
 import { Play } from 'phosphor-react'
-
 import {
   CountdownContainer,
   FormContainer,
@@ -9,25 +8,37 @@ import {
   StartCountdownButton,
   TaskInput,
 } from './style'
-
 export function Home() {
   return (
     <HomeContainer>
       <form>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Banana" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
         </FormContainer>
-
         <CountdownContainer>
           <span>0</span>
           <span>0</span>
@@ -35,7 +46,6 @@ export function Home() {
           <span>0</span>
           <span>0</span>
         </CountdownContainer>
-
         <StartCountdownButton disabled type="submit">
           <Play size={24} />
           Começar
